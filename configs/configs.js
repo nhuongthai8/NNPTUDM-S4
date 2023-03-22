8b8c1ab5b7c9c73b39114a7956e28783463ad92a
nguyenthanhtunghutechsg <nguyenthanhtunghutechsg@gmail.com>
2023-03-22 11:23:24 +0700

    ma hoa password

--- configs/configs.js
+++ configs/configs.js
@@ -3,5 +3,6 @@ module.exports={
     DB_NAME: 'NodejsS4',
     item_Collection : 'item',
     career_Collection:'career',
-    user_Collection:'user'
+    user_Collection:'user',
+    saltRounds:10
 }
--- schemas/users.js
+++ schemas/users.js
@@ -1,9 +1,17 @@
 let mongoose = require('mongoose');
 let configs = require('../configs/configs');
+const bcrypt = require('bcrypt');
+
 let Schema = new mongoose.Schema({
 	username:String,
 	email:String,
 	roles:String,
 	password:String
 });
+
+Schema.pre('save',function(){
+	const salt =  bcrypt.genSaltSync(configs.saltRounds);
+	this.password = bcrypt.hashSync(this.password, salt);
+});
+
 module.exports = mongoose.model(configs.user_Collection, Schema);
